2507d3e7fe47c40528c66c92967dcb9b98a126e8
Michiel <michiel@kitania.be>
2019-05-28 16:46:10 +0200

    Added: checksum, update fields where necessary

--- src/ResourceSpace/Command/FillResourceSpaceCommand.php
+++ src/ResourceSpace/Command/FillResourceSpaceCommand.php
@@ -4,6 +4,7 @@ namespace App\ResourceSpace\Command;
 use DOMDocument;
 use DOMXPath;
 use Phpoaipmh\Endpoint;
+use Phpoaipmh\Exception\OaipmhException;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -20,6 +21,7 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
     private $metadataPrefix;
     private $dataDefinition;
     private $datahubEndpoint;
+    private $exifFields;
 
     protected function configure()
     {
@@ -44,6 +46,7 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
         $this->namespace = $this->getContainer()->getParameter('datahub.namespace');
         $this->metadataPrefix = $this->getContainer()->getParameter('datahub.metadataprefix');
         $this->dataDefinition = $this->getContainer()->getParameter('datahub.data_definition');
+        $this->exifFields = $this->getContainer()->getParameter('exif_fields');
 
         // Make sure the folder name ends with a trailing slash
         $folder = rtrim($folder, '/') . '/';
@@ -57,7 +60,6 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
 
 
         $this->resourceSpaceData = $this->getCurrentResourceSpaceData();
-        var_dump($this->resourceSpaceData);
 
 
         // Loop through all files in the folder
@@ -76,7 +78,7 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
             if ($isSupportedImage) {
                 $this->processImage($folder. $imageFile);
             } else {
-                // Log incorrect file extension
+                // TODO log incorrect file extension
             }
         }
     }
@@ -85,7 +87,9 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
     {
         $md5 = md5_file($image);
         $exifData = exif_read_data($image);
-        $workPid = $exifData['DocumentName'];
+
+        // The fields that we're interested in, for future reference
+/*        $workPid = $exifData['DocumentName'];
         $dataPid = $exifData['ImageDescription'];
         $fileName = $exifData['FileName'];
         $mimeType = $exifData['MimeType'];
@@ -93,45 +97,84 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
         $copyright = $exifData['Copyright'];
         $imageWidth = $exifData['ImageWidth'];
         $imageHeight = $exifData['ImageLength'];
-        $dateTime = $exifData['DateTime'];
+        $dateTime = $exifData['DateTime'];*/
+
+        // These fields don't seem to exist
 //        $description = $exifData['Description'];
 //        $megaPixels = $exifData['MegaPixels'];
 
-        $datahubData = array();
-
-
-        // Fetch the necessary data from the Datahub
-        if(!$this->datahubEndpoint)
-            $this->datahubEndpoint = Endpoint::build($this->datahubUrl);
-
-        $record = $this->datahubEndpoint->getRecord($dataPid, $this->metadataPrefix);
-        $data = $record->GetRecord->record->metadata->children($this->namespace, true);
-        $domDoc = new DOMDocument;
-        $domDoc->loadXML($data->asXML());
-        $xpath = new DOMXPath($domDoc);
-
-        foreach($this->dataDefinition as $key => $dataDef) {
-            // TODO we probably need to support all languages, but how do we do this in ResourceSpace?
-            $query = $this->buildXpath($dataDef['xpath'], 'nl');
-            $extracted = $xpath->query($query);
-            $value = null;
-            if($extracted) {
-                if(count($extracted) > 0) {
-                    foreach($extracted as $extr) {
-                        if($extr->nodeValue !== 'n/a') {
-                            $value = $extr->nodeValue;
-                        }
-                    }
-                }
-            }
-            if($value != null) {
-                $datahubData[$key] = $value;
+        $dataPid = null;
+        $newData = array('checksum' => $md5);
+        foreach($this->exifFields as $key => $field) {
+            $value = $exifData[$field['exif']];
+            $newData[$field['field']] = $value;
+            if($key == 'dataPid') {
+                $dataPid = $value;
             }
         }
 
-        // TODO compare the data in memory to the data in ResourceSpace
-        if(true) {
-            echo $this->importIntoResourceSpace(realpath($image));
+
+        if($dataPid != null) {
+            try {
+                // Fetch the necessary data from the Datahub
+                if (!$this->datahubEndpoint)
+                    $this->datahubEndpoint = Endpoint::build($this->datahubUrl);
+
+                $record = $this->datahubEndpoint->getRecord($dataPid, $this->metadataPrefix);
+                $data = $record->GetRecord->record->metadata->children($this->namespace, true);
+                $domDoc = new DOMDocument;
+                $domDoc->loadXML($data->asXML());
+                $xpath = new DOMXPath($domDoc);
+
+                foreach ($this->dataDefinition as $dataDef) {
+                    // TODO we probably need to support multiple languages, but how will we do this in ResourceSpace?
+                    $query = $this->buildXpath($dataDef['xpath'], 'nl');
+                    $extracted = $xpath->query($query);
+                    $value = null;
+                    if ($extracted) {
+                        if (count($extracted) > 0) {
+                            foreach ($extracted as $extr) {
+                                if ($extr->nodeValue !== 'n/a') {
+                                    $value = $extr->nodeValue;
+                                }
+                            }
+                        }
+                    }
+                    if ($value != null) {
+                        $newData[$dataDef['field']] = $value;
+                    }
+                }
+            }
+            catch(OaipmhException $e) {
+                echo $e . PHP_EOL;
+            }
+        }
+
+        $createNew = true;
+        foreach($this->resourceSpaceData as $id => $resourceSpaceData) {
+            // Find the matching resource
+            if($resourceSpaceData['checksum'] == $newData['checksum']) {
+                $createNew = false;
+                // Update fields in ResourceSpace where necessary
+                foreach($newData as $key => $value) {
+                    $update = false;
+                    if(!array_key_exists($key, $resourceSpaceData)) {
+                        $update = true;
+                    } else {
+                        if($resourceSpaceData[$key] != $value) {
+                            $update = true;
+                        }
+                    }
+                    if($update) {
+                        $this->updateResourceSpaceField($id, $key, $value);
+                    }
+                }
+                break;
+            }
+        }
+        if($createNew) {
+            $result = $this->importIntoResourceSpace(realpath($image), $newData);
+            //TODO log the result if something went wrong
         }
     }
 
@@ -139,15 +182,44 @@ class FillResourceSpaceCommand extends ContainerAwareCommand
     {
         $query = 'user=' . $this->apiUsername . '&function=do_search&param1=';
         $url = $this->apiUrl . '?' . $query . '&sign=' . $this->getSign($query);
+        $allResources = file_get_contents($url);
+        $resources = json_decode($allResources, true);
+
+        $data = array();
+        foreach($resources as $resource) {
+            $extracted = array();
+            $currentData = json_decode($this->getResourceInfo($resource['ref']), true);
+            foreach($currentData as $field) {
+                $extracted[$field['name']] = $field['value'];
+            }
+            $data[$resource['ref']] = $extracted;
+        }
+
+        return $data;
+    }
+
+    protected function getResourceInfo($id)
+    {
+        $query = 'user=' . $this->apiUsername . '&function=get_resource_field_data&param1=' . $id;
+        $url = $this->apiUrl . '?' . $query . '&sign=' . $this->getSign($query);
         $data = file_get_contents($url);
         return $data;
     }
 
-    protected function importIntoResourceSpace($image)
+    protected function importIntoResourceSpace($image, $newData)
     {
-        $query = 'user=' . $this->apiUsername . '&function=create_resource&param1=1&param2=0&param3=' . $image;
+        $query = 'user=' . $this->apiUsername . '&function=create_resource&param1=1&param2=0&param3=' . urlencode($image) . '&param4=&param5=&param6=&param7=' . urlencode(json_encode($newData));
         $url = $this->apiUrl . '?' . $query . '&sign=' . $this->getSign($query);
         $data = file_get_contents($url);
+        return $data;
+    }
+
+    protected function updateResourceSpaceField($id, $key, $value)
+    {
+        $query = 'user=' . $this->apiUsername . '&function=update_field&param1=' . $id . '&param2=' . $key . '&param3=' . urlencode($value);
+        $url = $this->apiUrl . '?' . $query . '&sign=' . $this->getSign($query);
+        $data = file_get_contents($url);
+        echo $data;
         return $data;
     }
 
