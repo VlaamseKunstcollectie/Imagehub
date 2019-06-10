86783ac88ca8f0ffe78e7db37f1ebf48fffa7acd
Michiel <michiel@kitania.be>
2019-06-10 13:34:20 +0200

    Added: basic homepage

--- src/AppBundle/Imagehub/Controller/DefaultController.php
+++ src/AppBundle/Imagehub/Controller/DefaultController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('index.html.twig', [
+            'documentCount' => '1337'
         ]);
     }
 }
