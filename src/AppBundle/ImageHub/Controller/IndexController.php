af27fde6fb7b6c4093d9365b916dbf630fcab8ad
Michiel <michiel@kitania.be>
2019-06-10 14:36:28 +0200

    Changed: index controller naming

--- src/AppBundle/ImageHub/Controller/IndexController.php
+++ src/AppBundle/ImageHub/Controller/IndexController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     /**
      * @Route("/", name="index")
