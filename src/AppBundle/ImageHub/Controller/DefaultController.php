e3dfc6f9c7a4cd8fde1554d9456c635153edd128
Michiel <michiel@kitania.be>
2019-06-10 14:34:10 +0200

    Added: basic CSS to homepage

--- src/AppBundle/ImageHub/Controller/DefaultController.php
+++ src/AppBundle/ImageHub/Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Imagehub\Controller;
+namespace AppBundle\ImageHub\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
