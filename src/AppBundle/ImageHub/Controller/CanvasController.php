4cb9e4ff5f45782acd2ac62502867c42ae0bb7cc
Michiel <michiel@kitania.be>
2019-07-05 15:18:54 +0200

    Fixed: wrong service url; fixed: CORS when serving manifests or canvases

--- src/AppBundle/ImageHub/Controller/CanvasController.php
+++ src/AppBundle/ImageHub/Controller/CanvasController.php
@@ -14,13 +14,20 @@ class CanvasController extends Controller
      */
     public function getCanvas(Request $request, $manifestId = '', $canvasIndex = '')
     {
+        // Make sure the service URL name ends with a trailing slash
+        $baseUrl = rtrim($this->getParameter('service_url'), '/') . '/';
+
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $canvases = $dm->createQueryBuilder('AppBundle\ImageHub\CanvasBundle\Document\Canvas')->field('canvasId')->equals($this->getParameter('service_url') . $manifestId . '/canvas/' . $canvasIndex . '.json')->getQuery()->execute();
+        $canvases = $dm->createQueryBuilder('AppBundle\ImageHub\CanvasBundle\Document\Canvas')->field('canvasId')->equals($baseUrl . $manifestId . '/canvas/' . $canvasIndex . '.json')->getQuery()->execute();
         if(count($canvases) > 0) {
             foreach($canvases as $canvas) {
                 $toServe = $canvas->getData();
             }
-            return new Response(json_encode(json_decode($toServe), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, array('Content-Type' => 'application/json'));
+            $headers = array(
+                'Content-Type' => 'application/json',
+                'Access-Control-Allow-Origin' => '*'
+            );
+            return new Response(json_encode(json_decode($toServe), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, $headers);
         } else {
             return new Response('Sorry, the requested document does not exist.', 404);
         }
--- src/AppBundle/ImageHub/Controller/ManifestController.php
+++ src/AppBundle/ImageHub/Controller/ManifestController.php
@@ -14,14 +14,21 @@ class ManifestController extends Controller
      */
     public function manifestAction(Request $request, $manifestId = '')
     {
+        // Make sure the service URL name ends with a trailing slash
+        $baseUrl = rtrim($this->getParameter('service_url'), '/') . '/';
+
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $manifests = $dm->createQueryBuilder('AppBundle\ImageHub\ManifestBundle\Document\Manifest')->field('manifestId')->equals($this->getParameter('service_url') . $manifestId . '/manifest.json')->getQuery()->execute();
+        $manifests = $dm->createQueryBuilder('AppBundle\ImageHub\ManifestBundle\Document\Manifest')->field('manifestId')->equals($baseUrl . $manifestId . '/manifest.json')->getQuery()->execute();
         $toServe = 'Sorry, the requested document does not exist.';
         if(count($manifests) > 0) {
             foreach($manifests as $manifest) {
                 $toServe = $manifest->getData();
             }
-            return new Response(json_encode(json_decode($toServe), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, array('Content-Type' => 'application/json'));
+            $headers = array(
+                'Content-Type' => 'application/json',
+                'Access-Control-Allow-Origin' => '*'
+            );
+            return new Response(json_encode(json_decode($toServe), JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE), 200, $headers);
         } else {
             return new Response('Sorry, the requested document does not exist.', 404);
         }
