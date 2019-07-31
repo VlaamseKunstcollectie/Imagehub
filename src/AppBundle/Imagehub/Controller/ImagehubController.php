2c9f4adc9df4d220cb3b74a13fa13ef323e306b5
Michiel <michiel@kitania.be>
2019-07-31 15:47:50 +0200

    Added: home page

--- src/AppBundle/Imagehub/Controller/ImagehubController.php
+++ src/AppBundle/Imagehub/Controller/ImagehubController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IIIFController extends Controller
+class ImagehubController extends Controller
 {
     /**
      * @Route("/imagehub", name="imagehub")
      */
-    public function iiifAction(Request $request)
+    public function imagehubAction(Request $request)
     {
         return $this->render('imagehub.html.twig', [
             'current_page' => 'imagehub'
